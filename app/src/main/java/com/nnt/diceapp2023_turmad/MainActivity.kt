3473b55973c6c72d5668f8865897d7214c823604
Caio H Perlin <caiohperlin@gmail.com>
2023-07-06 18:30:02 -0400

    Animação dos dados e Binding

--- app/src/main/java/com/nnt/diceapp2023_turmad/MainActivity.kt
+++ app/src/main/java/com/nnt/diceapp2023_turmad/MainActivity.kt
@@ -6,115 +6,135 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.android.material.chip.ChipGroup
+import com.nnt.diceapp2023_turmad.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var animando = false
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val grupoDeDados = findViewById<ChipGroup>(R.id.chipGroup)
-        val botao = findViewById<Button>(R.id.botao)
-        val imagem = findViewById<ImageView>(R.id.imageView)
+        binding.botao.setOnClickListener {lancarDado(binding.chipGroup, binding.imageView)}
+    }
 
-        botao.setOnClickListener {lancarDado(grupoDeDados, imagem)}
+    private fun atualizarImagem(id: Int) {
+        if(!animando){
+            animando = true
+            binding.imageView.animate().apply {
+                duration = 200
+                scaleX(0f)
+                translationY(0f)
+            }.withEndAction {
+                binding.imageView.animate().apply {
+                    duration = 100
+                    scaleX(1f)
+                    translationY(100f)
+                }
+                binding.imageView.setImageResource(id)
+                animando = false
+            }.start()
+        }
     }
 
     private fun lancarDado(grupoDeDados: ChipGroup, imagem: ImageView) {
         val faceSorteada: Int
         when(grupoDeDados.checkedChipId) {
             R.id.d1 -> {
-                imagem.setImageResource(R.drawable.d1)
+                atualizarImagem(R.drawable.d1)
             }
             R.id.d4 -> {
                 faceSorteada = (1..4).random()
                 when(faceSorteada){
-                    1 -> imagem.setImageResource(R.drawable.d4_1)
-                    2 -> imagem.setImageResource(R.drawable.d4_2)
-                    3 -> imagem.setImageResource(R.drawable.d4_3)
-                    4 -> imagem.setImageResource(R.drawable.d4_4)
+                    1 -> atualizarImagem(R.drawable.d4_1)
+                    2 -> atualizarImagem(R.drawable.d4_2)
+                    3 -> atualizarImagem(R.drawable.d4_3)
+                    4 -> atualizarImagem(R.drawable.d4_4)
                 }
             }
             R.id.d6 -> {
                 faceSorteada = (1..6).random()
                 when(faceSorteada){
-                    1 -> imagem.setImageResource(R.drawable.d6_1)
-                    2 -> imagem.setImageResource(R.drawable.d6_2)
-                    3 -> imagem.setImageResource(R.drawable.d6_3)
-                    4 -> imagem.setImageResource(R.drawable.d6_4)
-                    5 -> imagem.setImageResource(R.drawable.d6_5)
-                    6 -> imagem.setImageResource(R.drawable.d6_6)
+                    1 -> atualizarImagem(R.drawable.d6_1)
+                    2 -> atualizarImagem(R.drawable.d6_2)
+                    3 -> atualizarImagem(R.drawable.d6_3)
+                    4 -> atualizarImagem(R.drawable.d6_4)
+                    5 -> atualizarImagem(R.drawable.d6_5)
+                    6 -> atualizarImagem(R.drawable.d6_6)
                 }
             }
             R.id.d8 -> {
                 faceSorteada = (1..8).random()
                 when(faceSorteada){
-                    1 -> imagem.setImageResource(R.drawable.d8_1)
-                    2 -> imagem.setImageResource(R.drawable.d8_2)
-                    3 -> imagem.setImageResource(R.drawable.d8_3)
-                    4 -> imagem.setImageResource(R.drawable.d8_4)
-                    5 -> imagem.setImageResource(R.drawable.d8_5)
-                    6 -> imagem.setImageResource(R.drawable.d8_6)
-                    7 -> imagem.setImageResource(R.drawable.d8_7)
-                    8 -> imagem.setImageResource(R.drawable.d8_8)
+                    1 -> atualizarImagem(R.drawable.d8_1)
+                    2 -> atualizarImagem(R.drawable.d8_2)
+                    3 -> atualizarImagem(R.drawable.d8_3)
+                    4 -> atualizarImagem(R.drawable.d8_4)
+                    5 -> atualizarImagem(R.drawable.d8_5)
+                    6 -> atualizarImagem(R.drawable.d8_6)
+                    7 -> atualizarImagem(R.drawable.d8_7)
+                    8 -> atualizarImagem(R.drawable.d8_8)
                 }
             }
             R.id.d10 -> {
                 faceSorteada = (1..10).random()
                 when(faceSorteada){
-                    1 -> imagem.setImageResource(R.drawable.d10_1)
-                    2 -> imagem.setImageResource(R.drawable.d10_2)
-                    3 -> imagem.setImageResource(R.drawable.d10_3)
-                    4 -> imagem.setImageResource(R.drawable.d10_4)
-                    5 -> imagem.setImageResource(R.drawable.d10_5)
-                    6 -> imagem.setImageResource(R.drawable.d10_6)
-                    7 -> imagem.setImageResource(R.drawable.d10_7)
-                    8 -> imagem.setImageResource(R.drawable.d10_8)
-                    9 -> imagem.setImageResource(R.drawable.d10_9)
-                    10 -> imagem.setImageResource(R.drawable.d10_10)
+                    1 -> atualizarImagem(R.drawable.d10_1)
+                    2 -> atualizarImagem(R.drawable.d10_2)
+                    3 -> atualizarImagem(R.drawable.d10_3)
+                    4 -> atualizarImagem(R.drawable.d10_4)
+                    5 -> atualizarImagem(R.drawable.d10_5)
+                    6 -> atualizarImagem(R.drawable.d10_6)
+                    7 -> atualizarImagem(R.drawable.d10_7)
+                    8 -> atualizarImagem(R.drawable.d10_8)
+                    9 -> atualizarImagem(R.drawable.d10_9)
+                    10 -> atualizarImagem(R.drawable.d10_10)
                 }
             }
             R.id.d12 -> {
                 faceSorteada = (1..12).random()
                 when(faceSorteada){
-                    1 -> imagem.setImageResource(R.drawable.d12_1)
-                    2 -> imagem.setImageResource(R.drawable.d12_2)
-                    3 -> imagem.setImageResource(R.drawable.d12_3)
-                    4 -> imagem.setImageResource(R.drawable.d12_4)
-                    5 -> imagem.setImageResource(R.drawable.d12_5)
-                    6 -> imagem.setImageResource(R.drawable.d12_6)
-                    7 -> imagem.setImageResource(R.drawable.d12_7)
-                    8 -> imagem.setImageResource(R.drawable.d12_8)
-                    9 -> imagem.setImageResource(R.drawable.d12_9)
-                    10 -> imagem.setImageResource(R.drawable.d12_10)
-                    11 -> imagem.setImageResource(R.drawable.d12_11)
-                    12 -> imagem.setImageResource(R.drawable.d12_12)
+                    1 -> atualizarImagem(R.drawable.d12_1)
+                    2 -> atualizarImagem(R.drawable.d12_2)
+                    3 -> atualizarImagem(R.drawable.d12_3)
+                    4 -> atualizarImagem(R.drawable.d12_4)
+                    5 -> atualizarImagem(R.drawable.d12_5)
+                    6 -> atualizarImagem(R.drawable.d12_6)
+                    7 -> atualizarImagem(R.drawable.d12_7)
+                    8 -> atualizarImagem(R.drawable.d12_8)
+                    9 -> atualizarImagem(R.drawable.d12_9)
+                    10 -> atualizarImagem(R.drawable.d12_10)
+                    11 -> atualizarImagem(R.drawable.d12_11)
+                    12 -> atualizarImagem(R.drawable.d12_12)
                 }
             }
             R.id.d20 -> {
                 faceSorteada = (1..20).random()
                 when(faceSorteada){
-                    1 -> imagem.setImageResource(R.drawable.d20_1)
-                    2 -> imagem.setImageResource(R.drawable.d20_2)
-                    3 -> imagem.setImageResource(R.drawable.d20_3)
-                    4 -> imagem.setImageResource(R.drawable.d20_4)
-                    5 -> imagem.setImageResource(R.drawable.d20_5)
-                    6 -> imagem.setImageResource(R.drawable.d20_6)
-                    7 -> imagem.setImageResource(R.drawable.d20_7)
-                    8 -> imagem.setImageResource(R.drawable.d20_8)
-                    9 -> imagem.setImageResource(R.drawable.d20_9)
-                    10 -> imagem.setImageResource(R.drawable.d20_10)
-                    11 -> imagem.setImageResource(R.drawable.d20_11)
-                    12 -> imagem.setImageResource(R.drawable.d20_12)
-                    13 -> imagem.setImageResource(R.drawable.d20_13)
-                    14 -> imagem.setImageResource(R.drawable.d20_14)
-                    15 -> imagem.setImageResource(R.drawable.d20_15)
-                    16 -> imagem.setImageResource(R.drawable.d20_16)
-                    17 -> imagem.setImageResource(R.drawable.d20_17)
-                    18 -> imagem.setImageResource(R.drawable.d20_18)
-                    19 -> imagem.setImageResource(R.drawable.d20_19)
-                    20 -> imagem.setImageResource(R.drawable.d20_20)
+                    1 -> atualizarImagem(R.drawable.d20_1)
+                    2 -> atualizarImagem(R.drawable.d20_2)
+                    3 -> atualizarImagem(R.drawable.d20_3)
+                    4 -> atualizarImagem(R.drawable.d20_4)
+                    5 -> atualizarImagem(R.drawable.d20_5)
+                    6 -> atualizarImagem(R.drawable.d20_6)
+                    7 -> atualizarImagem(R.drawable.d20_7)
+                    8 -> atualizarImagem(R.drawable.d20_8)
+                    9 -> atualizarImagem(R.drawable.d20_9)
+                    10 -> atualizarImagem(R.drawable.d20_10)
+                    11 -> atualizarImagem(R.drawable.d20_11)
+                    12 -> atualizarImagem(R.drawable.d20_12)
+                    13 -> atualizarImagem(R.drawable.d20_13)
+                    14 -> atualizarImagem(R.drawable.d20_14)
+                    15 -> atualizarImagem(R.drawable.d20_15)
+                    16 -> atualizarImagem(R.drawable.d20_16)
+                    17 -> atualizarImagem(R.drawable.d20_17)
+                    18 -> atualizarImagem(R.drawable.d20_18)
+                    19 -> atualizarImagem(R.drawable.d20_19)
+                    20 -> atualizarImagem(R.drawable.d20_20)
                 }
             }
         }
